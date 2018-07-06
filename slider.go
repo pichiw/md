b9b049e7d21b322ca781f060e95c466092adcc0c
evandigby <evandigby@gmail.com>
2018-07-05 21:04:52 -0700

    adding some options to the slider

--- slider.go
+++ slider.go
@@ -1,7 +1,8 @@
 package md
 
 import (
-	"fmt"
+	"strconv"
+	"sync"
 
 	"github.com/gowasm/gopherwasm/js"
 	"github.com/gowasm/vecty"
@@ -9,10 +10,32 @@ import (
 	"github.com/gowasm/vecty/prop"
 )
 
+func NewSlider(opts SliderOptions) *Slider {
+	return &Slider{
+		opts: opts,
+	}
+}
+
+type OnSliderChange func(s *Slider)
+
+type SliderOptions struct {
+	Min   float64
+	Max   float64
+	Value float64
+
+	OnChange OnSliderChange
+}
 type Slider struct {
 	vecty.Core
 
-	Value int
+	opts      SliderOptions
+	optsMutex sync.Mutex
+}
+
+func (s *Slider) Value() float64 {
+	s.optsMutex.Lock()
+	defer s.optsMutex.Unlock()
+	return s.opts.Value
 }
 
 func (s *Slider) Mount() {
@@ -24,13 +47,24 @@ func (s *Slider) Mount() {
 	slider.Call("listen", "MDCSlider:change",
 		js.NewCallback(
 			func(vs []js.Value) {
-				fmt.Println("New Value:", slider.Get("value"))
+				s.optsMutex.Lock()
+				s.opts.Value = slider.Get("value").Float()
+				s.optsMutex.Unlock()
+
+				oc := s.opts.OnChange
+				if oc == nil {
+					return
+				}
+				oc(s)
 			},
 		),
 	)
 }
 
 func (s *Slider) Render() vecty.ComponentOrHTML {
+	minStr := strconv.FormatFloat(s.opts.Min, 'f', -1, 64)
+	maxStr := strconv.FormatFloat(s.opts.Max, 'f', -1, 64)
+	valueStr := strconv.FormatFloat(s.opts.Value, 'f', -1, 64)
 	return elem.Div(
 		elem.Div(
 			vecty.Markup(
@@ -42,9 +76,9 @@ func (s *Slider) Render() vecty.ComponentOrHTML {
 					vecty.Class("mdc-slider"),
 					vecty.Attribute("tabindex", "0"),
 					vecty.Attribute("role", "slider"),
-					vecty.Attribute("aria-valuemin", "0"),
-					vecty.Attribute("aria-valuemax", "50"),
-					vecty.Attribute("aria-valuenow", "20"),
+					vecty.Attribute("aria-valuemin", minStr),
+					vecty.Attribute("aria-valuemax", maxStr),
+					vecty.Attribute("aria-valuenow", valueStr),
 					vecty.Attribute("aria-label", "Select Value"),
 				),
 				elem.Div(
